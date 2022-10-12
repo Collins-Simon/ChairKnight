36b4f854e4a2e6ff3fded122e550326875a25798
John Flynn <flynnjohn@ecs.vuw.ac.nz>
2022-10-12 15:51:48 +1300

    #11, Made folder/file structure more consistent and scalable

---
+++ Chair Knight/Scenes/Characters/Player.gd
@@ -0,0 +1,38 @@
+extends RigidBody2D
+
+class_name Player
+
+
+export(float) var max_speed := 5000
+
+var prev_velocity = Vector2.ZERO
+
+signal attempted_peg(pos)
+
+
+func _ready() -> void:
+	self.gravity_scale = 0
+
+func _physics_process(delta: float) -> void:
+
+	var input_vector := Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector:
+		linear_velocity = linear_velocity.move_toward(input_vector * max_speed, delta * 2500)
+	#else:
+		#linear_velocity = linear_velocity.move_toward(Vector2.ZERO, delta * 300)
+	linear_velocity *= 0.95
+	prev_velocity = linear_velocity
+
+
+func _on_PegArea_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.is_pressed():
+		emit_signal("attempted_peg", get_global_mouse_position())
+
+func launch(target_pos : Vector2):
+	var diff := target_pos - global_position
+	var dist := diff.length()
+	apply_central_impulse(diff.normalized() * (3500 + dist * 5))
---
+++ Chair Knight/Scenes/Equipment/Rope.gd
@@ -0,0 +1,63 @@
+extends Node2D
+class_name Rope
+
+
+var link_scene = preload("res://Scenes/Equipment/RopeLink.tscn")
+var joint_scene = preload("res://Scenes/Equipment/RopeJoint.tscn")
+
+var links = []
+var link_length = 50
+var joints = []
+
+var start_body : PhysicsBody2D = null
+var end_body : PhysicsBody2D = null
+
+
+func init(start, end):
+	start_body = start
+	end_body = end
+
+func _ready():
+	var start_pos = start_body.global_position
+	var end_pos = end_body.global_position
+	#var angle = start_pos.angle_to(end_pos)
+	var dist = start_pos.distance_to(end_pos)
+	var num_links = floor(dist / link_length)
+	var num_joints = num_links + 1
+
+	var diff = start_pos - end_pos
+	var dist_scaled = num_links * link_length
+	var diff_scaled = diff * (dist_scaled / dist)
+	var start_pos_scaled = end_pos + diff_scaled
+
+	for i in range(num_links):
+		var link = link_scene.instance()
+		link.global_position = lerp(start_pos_scaled, end_pos, i / num_links)
+		links.append(link)
+		add_child(link)
+
+	links.push_front(start_body)
+	links.append(end_body)
+
+	for i in range(num_joints):
+		var joint = joint_scene.instance()
+		var pos
+		if i == 0: pos = start_pos
+		else: pos = lerp(start_pos_scaled, end_pos, (i-1) / (num_joints-1))
+		joint.global_position = pos
+		joint.rotation = pos.angle_to_point(links[i+1].global_position) + deg2rad(90)
+		#joint.rotate(-angle)
+		joints.append(joint)
+#		add_child(joint)
+
+	if num_joints == 1:
+		joints[0].disable_collision = false
+
+	for i in range(num_joints):
+		if i == 0: joints[i].set_links(links[i].get_path(), links[i+1].get_path(), dist - dist_scaled)
+		else: joints[i].set_links(links[i].get_path(), links[i+1].get_path(), link_length)
+		#joints[i].rotation = joints[i].global_position.angle_to_point(links[i+1].global_position) + deg2rad(90)
+		add_child(joints[i])
+
+func pull():
+	for joint in joints: joint.pull()
