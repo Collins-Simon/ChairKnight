b243e0ec7b6a34c8d42192d62b5ad30e93c1657e
John Flynn <flynnjohn@ecs.vuw.ac.nz>
2022-10-14 00:04:32 +1300

    #21, Add EnemyBig shockwave attack, and fix Rope deletion bug
    When close enough to the Player, EnemyBigs now use a close-range shockwave attack. There was also a bug that crashed the game when an Enemy died with multiple Ropes attached, but this has been fixed (deleted Ropes from list badly).

--- Chair Knight/Scenes/Characters/Enemies/EnemyBig.gd
+++ Chair Knight/Scenes/Characters/Enemies/EnemyBig.gd
@@ -2,12 +2,40 @@ extends Enemy
 class_name EnemyBig
 
 
+var charging_attack := false
+var attacking := false
+
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var attack_timer = $AttackTimer
+onready var anim_player = $AnimationPlayer
+onready var particles = $Particles2D
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2.ZERO
 	if playerDetectionZone.player_detected():
-		direction = global_position.direction_to(playerDetectionZone.player.global_position)
+		var player_pos = playerDetectionZone.player.global_position
+		direction = global_position.direction_to(player_pos)
+		particles.process_material.direction = Vector3(direction.x, direction.y, 0.0)
 
-	move(direction, delta)
+		if global_position.distance_to(player_pos) <= 400:
+			if not charging_attack and not attacking:
+				charging_attack = true
+				attack_timer.start()
+
+		elif charging_attack and not attacking:
+			charging_attack = false
+			attack_timer.stop()
+
+	if attacking: move(Vector2.ZERO, delta)
+	else: move(direction, delta)
+
+
+func _on_AttackTimer_timeout() -> void:
+	charging_attack = false
+	anim_player.play("Attack")
+	attacking = true
+
+func attack_finished() -> void:
+	charging_attack = false
+	attacking = false
