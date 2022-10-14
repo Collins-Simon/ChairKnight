43361c39ccc8ed65a7f0e60336f45ced44520551
John Flynn <flynnjohn@ecs.vuw.ac.nz>
2022-10-14 20:44:52 +1300

    #27, Move grappling methods from World to Player script
    This provides better encapsulation and declutters World.gd.

--- Chair Knight/Scenes/Characters/Player.gd
+++ Chair Knight/Scenes/Characters/Player.gd
@@ -2,6 +2,9 @@ extends Character
 class_name Player
 
 
+var rope_scene = preload("res://Scenes/Equipment/Rope.tscn")
+var grapple_rope: Rope = null
+
 onready var hitbox = $Hitbox
 onready var currentRoom = null
 
@@ -11,6 +14,26 @@ func _physics_process(delta: float) -> void:
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	move(input_vector, delta)
 	hitbox.damage = linear_velocity.length()
+
+
+func grapple(target: GrappleBody) -> void:
+	grapple_rope = rope_scene.instance()
+	grapple_rope.init(self, target)
+
+func is_grappled() -> bool:
+	return grapple_rope != null
+
+func ungrapple() -> void:
+	if grapple_rope == null: return
+	grapple_rope.destroy()
+	grapple_rope = null
+
+func grapple_launch() -> void:
+	if grapple_rope == null: return
+	var end_body = grapple_rope.end_body
+	ungrapple()
+	launch(end_body.global_position)
+
 
 func launch(target_pos : Vector2) -> void:
 	var diff := target_pos - global_position
--- Chair Knight/Scenes/World.gd
+++ Chair Knight/Scenes/World.gd
@@ -5,10 +5,9 @@ var rope_scene = preload("res://Scenes/Equipment/Rope.tscn")
 var bullet_scene = preload("res://Scenes/Equipment/Bullet.tscn")
 var explosion_scene = preload("res://Scenes/Equipment/Explosion.tscn")
 
-onready var player = $"%Player"
+onready var player: Player = $"%Player"
 onready var entities = $"%Entities"
 onready var ropes = $"%Ropes"
-onready var grapple_rope: Rope = null;
 
 signal room_cleared
 
@@ -44,6 +43,7 @@ func shoot_bullet(shooter: Node2D, velocity: Vector2) -> void:
 	bullet.init(shooter.global_position, velocity, shooter is Enemy)
 	entities.add_child(bullet)
 
+
 func connect_grapple_body(body: GrappleBody) -> void:
 	body.connect("clicked", self, "_on_GrappleBody_clicked")
 	body.connect("destroyed", self, "_on_GrappleBody_destroyed")
@@ -59,38 +59,23 @@ func _on_GrappleBody_destroyed(body: GrappleBody) -> void:
 	var attached_ropes := body.get_attached_ropes()
 	for i in range(attached_ropes.size()-1, -1, -1):
 		var rope: Rope = attached_ropes[i]
-		if rope == grapple_rope: grapple_rope = null
-		destroy_rope(rope)
+		if rope == player.grapple_rope: player.ungrapple()
+		else: rope.destroy()
 	body.queue_free()
 
 func _on_GrappleBody_clicked(left_click: bool, target: GrappleBody):
 	if left_click:
-		if grapple_rope != null: return
-		grapple_rope = rope_scene.instance()
-		grapple_rope.init(player, target)
-		ropes.add_child(grapple_rope)
+		if player.is_grappled(): return
+		player.grapple(target)
+		ropes.add_child(player.grapple_rope)
 
 	else:
-		if grapple_rope == null: return
-		var end_body = grapple_rope.end_body
+		if not player.is_grappled(): return
+		var end_body = player.grapple_rope.end_body
 		if end_body == target: return
-		if target.global_position.distance_squared_to(player.global_position) > 100_000: return
-		ungrapple()
+		if target.global_position.distance_to(player.global_position) > 300: return
+		player.ungrapple()
 
 		var detached_rope = rope_scene.instance()
 		detached_rope.init(target, end_body)
 		ropes.add_child(detached_rope)
-
-func ungrapple():
-	if grapple_rope == null: return
-	destroy_rope(grapple_rope)
-	grapple_rope = null
-
-func destroy_rope(rope: Rope):
-	rope.destroy()
-
-func attempt_rope_launch():
-	if grapple_rope == null: return
-	var end_body = grapple_rope.end_body
-	ungrapple()
-	player.launch(end_body.global_position)
