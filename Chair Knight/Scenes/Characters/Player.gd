2b412120fd58d53fa234bb8b9a21059376a2c6d8
Simon Collins <collinsimo@ecs.vuw.ac.nz>
2022-10-15 13:46:09 +1300

    #25 more sprite work

--- Chair Knight/Scenes/Characters/Player.gd
+++ Chair Knight/Scenes/Characters/Player.gd
@@ -4,6 +4,15 @@ class_name Player
 
 onready var hitbox = $Hitbox
 onready var currentRoom = null
+onready var anim_sprite = $AnimatedSprite
+var diving = false;
+
+func _on_AnimatedSprite_animation_finished():
+	if(anim_sprite.get_animation() == "Dive"):
+		diving = false
+		anim_sprite.set_animation("Walk")
+
+
 
 func _physics_process(delta: float) -> void:
 	var input_vector := Vector2.ZERO
@@ -11,8 +20,17 @@ func _physics_process(delta: float) -> void:
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	move(input_vector, delta)
 	hitbox.damage = linear_velocity.length()
+	
+	#Handle animations
+	if(linear_velocity.x > 0):
+		anim_sprite.flip_h = false
+	else:
+		anim_sprite.flip_h = true
+	
 
 func launch(target_pos : Vector2) -> void:
+	anim_sprite.set_animation("Dive")
+	diving = true
 	var diff := target_pos - global_position
 	var dist := diff.length()
 	apply_central_impulse(diff.normalized() * (3500 + dist * 5))
